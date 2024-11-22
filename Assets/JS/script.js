27230fcacba3ce7d4a91c80cd189df2335cabe6a
LidiaMargaritaCV <lidiamargaritaceballos@gmail.com>
2024-11-22 17:18:53 -0600

    filtro

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,1 +1,22 @@
-console.log("¡Hola desde script.js!");
+document.addEventListener("DOMContentLoaded", () => {
+    const barraBusqueda = document.getElementById("barraBusqueda");
+    const searchModal = document.getElementById("searchModal");
+    const closeModal = document.getElementById("closeModal");
+
+    // Abrir modal
+    barraBusqueda.addEventListener("click", () => {
+        searchModal.classList.add("active");
+    });
+
+    // Cerrar modal
+    closeModal.addEventListener("click", () => {
+        searchModal.classList.remove("active");
+    });
+
+    // Cerrar modal al hacer clic fuera
+    searchModal.addEventListener("click", (event) => {
+        if (event.target === searchModal) {
+            searchModal.classList.remove("active");
+        }
+    });
+});
